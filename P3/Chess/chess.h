1d0cf48713948c3b284a89f7ffaead0ff6d300c0
githubnemanja <githubnemanja@users.noreply.github.com>
2018-12-31 20:13:02 +0100

    Add files via upload

---
+++ P3/Chess/chess.h
@@ -0,0 +1,127 @@
+#ifndef CHESS_H
+#define CHESS_H
+
+#include <QWidget>
+#include "figura.h"
+#include <QLabel>
+#include <QMap>
+#include <QPushButton>
+#include "stanje.h"
+#include <QPainter>
+
+namespace Ui {
+class Chess;
+}
+
+class Chess : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Chess(QWidget *parent = nullptr);
+    ~Chess();
+
+private slots:
+
+    void on_pushButton_00_clicked();
+    void on_pushButton_01_clicked();
+    void on_pushButton_02_clicked();
+    void on_pushButton_03_clicked();
+    void on_pushButton_04_clicked();
+    void on_pushButton_05_clicked();
+    void on_pushButton_06_clicked();
+    void on_pushButton_07_clicked();
+    void on_pushButton_08_clicked();
+    void on_pushButton_09_clicked();
+    void on_pushButton_10_clicked();
+    void on_pushButton_11_clicked();
+    void on_pushButton_12_clicked();
+    void on_pushButton_13_clicked();
+    void on_pushButton_14_clicked();
+    void on_pushButton_15_clicked();
+    void on_pushButton_16_clicked();
+    void on_pushButton_17_clicked();
+    void on_pushButton_18_clicked();
+    void on_pushButton_19_clicked();
+    void on_pushButton_20_clicked();
+    void on_pushButton_21_clicked();
+    void on_pushButton_22_clicked();
+    void on_pushButton_23_clicked();
+    void on_pushButton_24_clicked();
+    void on_pushButton_25_clicked();
+    void on_pushButton_26_clicked();
+    void on_pushButton_27_clicked();
+    void on_pushButton_28_clicked();
+    void on_pushButton_29_clicked();
+    void on_pushButton_30_clicked();
+    void on_pushButton_31_clicked();
+    void on_pushButton_32_clicked();
+    void on_pushButton_33_clicked();
+    void on_pushButton_34_clicked();
+    void on_pushButton_35_clicked();
+    void on_pushButton_36_clicked();
+    void on_pushButton_37_clicked();
+    void on_pushButton_38_clicked();
+    void on_pushButton_39_clicked();
+    void on_pushButton_40_clicked();
+    void on_pushButton_41_clicked();
+    void on_pushButton_42_clicked();
+    void on_pushButton_43_clicked();
+    void on_pushButton_44_clicked();
+    void on_pushButton_45_clicked();
+    void on_pushButton_46_clicked();
+    void on_pushButton_47_clicked();
+    void on_pushButton_48_clicked();
+    void on_pushButton_49_clicked();
+    void on_pushButton_50_clicked();
+    void on_pushButton_51_clicked();
+    void on_pushButton_52_clicked();
+    void on_pushButton_53_clicked();
+    void on_pushButton_54_clicked();
+    void on_pushButton_55_clicked();
+    void on_pushButton_56_clicked();
+    void on_pushButton_57_clicked();
+    void on_pushButton_58_clicked();
+    void on_pushButton_59_clicked();
+    void on_pushButton_60_clicked();
+    void on_pushButton_61_clicked();
+    void on_pushButton_62_clicked();
+    void on_pushButton_63_clicked();
+
+
+private:
+    Ui::Chess *ui;
+
+    QMap<int,Figura*> mapa;
+    Pijun *p0,*p1;
+    Top *t0,*t1;
+    Konj *k0,*k1;
+    Lovac *l0,*l1;
+    Kraljica *y0,*y1;
+    Kralj *x0,*x1;
+    QStanje stanje;
+    int pKlik;
+    bool* polja;
+    int brojPoteza;
+
+
+    void inicijalizujTablu();
+    int labelToInt(QLabel*)const;
+    QLabel* intToLabel(int)const;
+    QPushButton* intToButton(int)const;
+    void potez(int);
+    void crtaj(Figura*, QLabel*);
+    void figuraNapadaPolja(int,bool*);
+    bool figuraNapadaPolja(int);
+    void allButtonsDisable();
+    void changeButtons(int);
+    void setPolja();
+    void Polja();
+    void priprema();
+    bool movable(int);
+    void crtajKvadrat(int);
+    QString intToChar(int);
+};
+
+
+#endif // CHESS_H
---
+++ P3/build-Chess-Desktop_Qt_5_12_0_MinGW_64_bit-Debug/debug/moc_chess.cpp
@@ -0,0 +1,433 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'chess.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.12.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../Chess/chess.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'chess.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.12.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_Chess_t {
+    QByteArrayData data[66];
+    char stringdata0[1607];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_Chess_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_Chess_t qt_meta_stringdata_Chess = {
+    {
+QT_MOC_LITERAL(0, 0, 5), // "Chess"
+QT_MOC_LITERAL(1, 6, 24), // "on_pushButton_00_clicked"
+QT_MOC_LITERAL(2, 31, 0), // ""
+QT_MOC_LITERAL(3, 32, 24), // "on_pushButton_01_clicked"
+QT_MOC_LITERAL(4, 57, 24), // "on_pushButton_02_clicked"
+QT_MOC_LITERAL(5, 82, 24), // "on_pushButton_03_clicked"
+QT_MOC_LITERAL(6, 107, 24), // "on_pushButton_04_clicked"
+QT_MOC_LITERAL(7, 132, 24), // "on_pushButton_05_clicked"
+QT_MOC_LITERAL(8, 157, 24), // "on_pushButton_06_clicked"
+QT_MOC_LITERAL(9, 182, 24), // "on_pushButton_07_clicked"
+QT_MOC_LITERAL(10, 207, 24), // "on_pushButton_08_clicked"
+QT_MOC_LITERAL(11, 232, 24), // "on_pushButton_09_clicked"
+QT_MOC_LITERAL(12, 257, 24), // "on_pushButton_10_clicked"
+QT_MOC_LITERAL(13, 282, 24), // "on_pushButton_11_clicked"
+QT_MOC_LITERAL(14, 307, 24), // "on_pushButton_12_clicked"
+QT_MOC_LITERAL(15, 332, 24), // "on_pushButton_13_clicked"
+QT_MOC_LITERAL(16, 357, 24), // "on_pushButton_14_clicked"
+QT_MOC_LITERAL(17, 382, 24), // "on_pushButton_15_clicked"
+QT_MOC_LITERAL(18, 407, 24), // "on_pushButton_16_clicked"
+QT_MOC_LITERAL(19, 432, 24), // "on_pushButton_17_clicked"
+QT_MOC_LITERAL(20, 457, 24), // "on_pushButton_18_clicked"
+QT_MOC_LITERAL(21, 482, 24), // "on_pushButton_19_clicked"
+QT_MOC_LITERAL(22, 507, 24), // "on_pushButton_20_clicked"
+QT_MOC_LITERAL(23, 532, 24), // "on_pushButton_21_clicked"
+QT_MOC_LITERAL(24, 557, 24), // "on_pushButton_22_clicked"
+QT_MOC_LITERAL(25, 582, 24), // "on_pushButton_23_clicked"
+QT_MOC_LITERAL(26, 607, 24), // "on_pushButton_24_clicked"
+QT_MOC_LITERAL(27, 632, 24), // "on_pushButton_25_clicked"
+QT_MOC_LITERAL(28, 657, 24), // "on_pushButton_26_clicked"
+QT_MOC_LITERAL(29, 682, 24), // "on_pushButton_27_clicked"
+QT_MOC_LITERAL(30, 707, 24), // "on_pushButton_28_clicked"
+QT_MOC_LITERAL(31, 732, 24), // "on_pushButton_29_clicked"
+QT_MOC_LITERAL(32, 757, 24), // "on_pushButton_30_clicked"
+QT_MOC_LITERAL(33, 782, 24), // "on_pushButton_31_clicked"
+QT_MOC_LITERAL(34, 807, 24), // "on_pushButton_32_clicked"
+QT_MOC_LITERAL(35, 832, 24), // "on_pushButton_33_clicked"
+QT_MOC_LITERAL(36, 857, 24), // "on_pushButton_34_clicked"
+QT_MOC_LITERAL(37, 882, 24), // "on_pushButton_35_clicked"
+QT_MOC_LITERAL(38, 907, 24), // "on_pushButton_36_clicked"
+QT_MOC_LITERAL(39, 932, 24), // "on_pushButton_37_clicked"
+QT_MOC_LITERAL(40, 957, 24), // "on_pushButton_38_clicked"
+QT_MOC_LITERAL(41, 982, 24), // "on_pushButton_39_clicked"
+QT_MOC_LITERAL(42, 1007, 24), // "on_pushButton_40_clicked"
+QT_MOC_LITERAL(43, 1032, 24), // "on_pushButton_41_clicked"
+QT_MOC_LITERAL(44, 1057, 24), // "on_pushButton_42_clicked"
+QT_MOC_LITERAL(45, 1082, 24), // "on_pushButton_43_clicked"
+QT_MOC_LITERAL(46, 1107, 24), // "on_pushButton_44_clicked"
+QT_MOC_LITERAL(47, 1132, 24), // "on_pushButton_45_clicked"
+QT_MOC_LITERAL(48, 1157, 24), // "on_pushButton_46_clicked"
+QT_MOC_LITERAL(49, 1182, 24), // "on_pushButton_47_clicked"
+QT_MOC_LITERAL(50, 1207, 24), // "on_pushButton_48_clicked"
+QT_MOC_LITERAL(51, 1232, 24), // "on_pushButton_49_clicked"
+QT_MOC_LITERAL(52, 1257, 24), // "on_pushButton_50_clicked"
+QT_MOC_LITERAL(53, 1282, 24), // "on_pushButton_51_clicked"
+QT_MOC_LITERAL(54, 1307, 24), // "on_pushButton_52_clicked"
+QT_MOC_LITERAL(55, 1332, 24), // "on_pushButton_53_clicked"
+QT_MOC_LITERAL(56, 1357, 24), // "on_pushButton_54_clicked"
+QT_MOC_LITERAL(57, 1382, 24), // "on_pushButton_55_clicked"
+QT_MOC_LITERAL(58, 1407, 24), // "on_pushButton_56_clicked"
+QT_MOC_LITERAL(59, 1432, 24), // "on_pushButton_57_clicked"
+QT_MOC_LITERAL(60, 1457, 24), // "on_pushButton_58_clicked"
+QT_MOC_LITERAL(61, 1482, 24), // "on_pushButton_59_clicked"
+QT_MOC_LITERAL(62, 1507, 24), // "on_pushButton_60_clicked"
+QT_MOC_LITERAL(63, 1532, 24), // "on_pushButton_61_clicked"
+QT_MOC_LITERAL(64, 1557, 24), // "on_pushButton_62_clicked"
+QT_MOC_LITERAL(65, 1582, 24) // "on_pushButton_63_clicked"
+
+    },
+    "Chess\0on_pushButton_00_clicked\0\0"
+    "on_pushButton_01_clicked\0"
+    "on_pushButton_02_clicked\0"
+    "on_pushButton_03_clicked\0"
+    "on_pushButton_04_clicked\0"
+    "on_pushButton_05_clicked\0"
+    "on_pushButton_06_clicked\0"
+    "on_pushButton_07_clicked\0"
+    "on_pushButton_08_clicked\0"
+    "on_pushButton_09_clicked\0"
+    "on_pushButton_10_clicked\0"
+    "on_pushButton_11_clicked\0"
+    "on_pushButton_12_clicked\0"
+    "on_pushButton_13_clicked\0"
+    "on_pushButton_14_clicked\0"
+    "on_pushButton_15_clicked\0"
+    "on_pushButton_16_clicked\0"
+    "on_pushButton_17_clicked\0"
+    "on_pushButton_18_clicked\0"
+    "on_pushButton_19_clicked\0"
+    "on_pushButton_20_clicked\0"
+    "on_pushButton_21_clicked\0"
+    "on_pushButton_22_clicked\0"
+    "on_pushButton_23_clicked\0"
+    "on_pushButton_24_clicked\0"
+    "on_pushButton_25_clicked\0"
+    "on_pushButton_26_clicked\0"
+    "on_pushButton_27_clicked\0"
+    "on_pushButton_28_clicked\0"
+    "on_pushButton_29_clicked\0"
+    "on_pushButton_30_clicked\0"
+    "on_pushButton_31_clicked\0"
+    "on_pushButton_32_clicked\0"
+    "on_pushButton_33_clicked\0"
+    "on_pushButton_34_clicked\0"
+    "on_pushButton_35_clicked\0"
+    "on_pushButton_36_clicked\0"
+    "on_pushButton_37_clicked\0"
+    "on_pushButton_38_clicked\0"
+    "on_pushButton_39_clicked\0"
+    "on_pushButton_40_clicked\0"
+    "on_pushButton_41_clicked\0"
+    "on_pushButton_42_clicked\0"
+    "on_pushButton_43_clicked\0"
+    "on_pushButton_44_clicked\0"
+    "on_pushButton_45_clicked\0"
+    "on_pushButton_46_clicked\0"
+    "on_pushButton_47_clicked\0"
+    "on_pushButton_48_clicked\0"
+    "on_pushButton_49_clicked\0"
+    "on_pushButton_50_clicked\0"
+    "on_pushButton_51_clicked\0"
+    "on_pushButton_52_clicked\0"
+    "on_pushButton_53_clicked\0"
+    "on_pushButton_54_clicked\0"
+    "on_pushButton_55_clicked\0"
+    "on_pushButton_56_clicked\0"
+    "on_pushButton_57_clicked\0"
+    "on_pushButton_58_clicked\0"
+    "on_pushButton_59_clicked\0"
+    "on_pushButton_60_clicked\0"
+    "on_pushButton_61_clicked\0"
+    "on_pushButton_62_clicked\0"
+    "on_pushButton_63_clicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_Chess[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      64,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  334,    2, 0x08 /* Private */,
+       3,    0,  335,    2, 0x08 /* Private */,
+       4,    0,  336,    2, 0x08 /* Private */,
+       5,    0,  337,    2, 0x08 /* Private */,
+       6,    0,  338,    2, 0x08 /* Private */,
+       7,    0,  339,    2, 0x08 /* Private */,
+       8,    0,  340,    2, 0x08 /* Private */,
+       9,    0,  341,    2, 0x08 /* Private */,
+      10,    0,  342,    2, 0x08 /* Private */,
+      11,    0,  343,    2, 0x08 /* Private */,
+      12,    0,  344,    2, 0x08 /* Private */,
+      13,    0,  345,    2, 0x08 /* Private */,
+      14,    0,  346,    2, 0x08 /* Private */,
+      15,    0,  347,    2, 0x08 /* Private */,
+      16,    0,  348,    2, 0x08 /* Private */,
+      17,    0,  349,    2, 0x08 /* Private */,
+      18,    0,  350,    2, 0x08 /* Private */,
+      19,    0,  351,    2, 0x08 /* Private */,
+      20,    0,  352,    2, 0x08 /* Private */,
+      21,    0,  353,    2, 0x08 /* Private */,
+      22,    0,  354,    2, 0x08 /* Private */,
+      23,    0,  355,    2, 0x08 /* Private */,
+      24,    0,  356,    2, 0x08 /* Private */,
+      25,    0,  357,    2, 0x08 /* Private */,
+      26,    0,  358,    2, 0x08 /* Private */,
+      27,    0,  359,    2, 0x08 /* Private */,
+      28,    0,  360,    2, 0x08 /* Private */,
+      29,    0,  361,    2, 0x08 /* Private */,
+      30,    0,  362,    2, 0x08 /* Private */,
+      31,    0,  363,    2, 0x08 /* Private */,
+      32,    0,  364,    2, 0x08 /* Private */,
+      33,    0,  365,    2, 0x08 /* Private */,
+      34,    0,  366,    2, 0x08 /* Private */,
+      35,    0,  367,    2, 0x08 /* Private */,
+      36,    0,  368,    2, 0x08 /* Private */,
+      37,    0,  369,    2, 0x08 /* Private */,
+      38,    0,  370,    2, 0x08 /* Private */,
+      39,    0,  371,    2, 0x08 /* Private */,
+      40,    0,  372,    2, 0x08 /* Private */,
+      41,    0,  373,    2, 0x08 /* Private */,
+      42,    0,  374,    2, 0x08 /* Private */,
+      43,    0,  375,    2, 0x08 /* Private */,
+      44,    0,  376,    2, 0x08 /* Private */,
+      45,    0,  377,    2, 0x08 /* Private */,
+      46,    0,  378,    2, 0x08 /* Private */,
+      47,    0,  379,    2, 0x08 /* Private */,
+      48,    0,  380,    2, 0x08 /* Private */,
+      49,    0,  381,    2, 0x08 /* Private */,
+      50,    0,  382,    2, 0x08 /* Private */,
+      51,    0,  383,    2, 0x08 /* Private */,
+      52,    0,  384,    2, 0x08 /* Private */,
+      53,    0,  385,    2, 0x08 /* Private */,
+      54,    0,  386,    2, 0x08 /* Private */,
+      55,    0,  387,    2, 0x08 /* Private */,
+      56,    0,  388,    2, 0x08 /* Private */,
+      57,    0,  389,    2, 0x08 /* Private */,
+      58,    0,  390,    2, 0x08 /* Private */,
+      59,    0,  391,    2, 0x08 /* Private */,
+      60,    0,  392,    2, 0x08 /* Private */,
+      61,    0,  393,    2, 0x08 /* Private */,
+      62,    0,  394,    2, 0x08 /* Private */,
+      63,    0,  395,    2, 0x08 /* Private */,
+      64,    0,  396,    2, 0x08 /* Private */,
+      65,    0,  397,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void Chess::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        Chess *_t = static_cast<Chess *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_pushButton_00_clicked(); break;
+        case 1: _t->on_pushButton_01_clicked(); break;
+        case 2: _t->on_pushButton_02_clicked(); break;
+        case 3: _t->on_pushButton_03_clicked(); break;
+        case 4: _t->on_pushButton_04_clicked(); break;
+        case 5: _t->on_pushButton_05_clicked(); break;
+        case 6: _t->on_pushButton_06_clicked(); break;
+        case 7: _t->on_pushButton_07_clicked(); break;
+        case 8: _t->on_pushButton_08_clicked(); break;
+        case 9: _t->on_pushButton_09_clicked(); break;
+        case 10: _t->on_pushButton_10_clicked(); break;
+        case 11: _t->on_pushButton_11_clicked(); break;
+        case 12: _t->on_pushButton_12_clicked(); break;
+        case 13: _t->on_pushButton_13_clicked(); break;
+        case 14: _t->on_pushButton_14_clicked(); break;
+        case 15: _t->on_pushButton_15_clicked(); break;
+        case 16: _t->on_pushButton_16_clicked(); break;
+        case 17: _t->on_pushButton_17_clicked(); break;
+        case 18: _t->on_pushButton_18_clicked(); break;
+        case 19: _t->on_pushButton_19_clicked(); break;
+        case 20: _t->on_pushButton_20_clicked(); break;
+        case 21: _t->on_pushButton_21_clicked(); break;
+        case 22: _t->on_pushButton_22_clicked(); break;
+        case 23: _t->on_pushButton_23_clicked(); break;
+        case 24: _t->on_pushButton_24_clicked(); break;
+        case 25: _t->on_pushButton_25_clicked(); break;
+        case 26: _t->on_pushButton_26_clicked(); break;
+        case 27: _t->on_pushButton_27_clicked(); break;
+        case 28: _t->on_pushButton_28_clicked(); break;
+        case 29: _t->on_pushButton_29_clicked(); break;
+        case 30: _t->on_pushButton_30_clicked(); break;
+        case 31: _t->on_pushButton_31_clicked(); break;
+        case 32: _t->on_pushButton_32_clicked(); break;
+        case 33: _t->on_pushButton_33_clicked(); break;
+        case 34: _t->on_pushButton_34_clicked(); break;
+        case 35: _t->on_pushButton_35_clicked(); break;
+        case 36: _t->on_pushButton_36_clicked(); break;
+        case 37: _t->on_pushButton_37_clicked(); break;
+        case 38: _t->on_pushButton_38_clicked(); break;
+        case 39: _t->on_pushButton_39_clicked(); break;
+        case 40: _t->on_pushButton_40_clicked(); break;
+        case 41: _t->on_pushButton_41_clicked(); break;
+        case 42: _t->on_pushButton_42_clicked(); break;
+        case 43: _t->on_pushButton_43_clicked(); break;
+        case 44: _t->on_pushButton_44_clicked(); break;
+        case 45: _t->on_pushButton_45_clicked(); break;
+        case 46: _t->on_pushButton_46_clicked(); break;
+        case 47: _t->on_pushButton_47_clicked(); break;
+        case 48: _t->on_pushButton_48_clicked(); break;
+        case 49: _t->on_pushButton_49_clicked(); break;
+        case 50: _t->on_pushButton_50_clicked(); break;
+        case 51: _t->on_pushButton_51_clicked(); break;
+        case 52: _t->on_pushButton_52_clicked(); break;
+        case 53: _t->on_pushButton_53_clicked(); break;
+        case 54: _t->on_pushButton_54_clicked(); break;
+        case 55: _t->on_pushButton_55_clicked(); break;
+        case 56: _t->on_pushButton_56_clicked(); break;
+        case 57: _t->on_pushButton_57_clicked(); break;
+        case 58: _t->on_pushButton_58_clicked(); break;
+        case 59: _t->on_pushButton_59_clicked(); break;
+        case 60: _t->on_pushButton_60_clicked(); break;
+        case 61: _t->on_pushButton_61_clicked(); break;
+        case 62: _t->on_pushButton_62_clicked(); break;
+        case 63: _t->on_pushButton_63_clicked(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+QT_INIT_METAOBJECT const QMetaObject Chess::staticMetaObject = { {
+    &QWidget::staticMetaObject,
+    qt_meta_stringdata_Chess.data,
+    qt_meta_data_Chess,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *Chess::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Chess::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_Chess.stringdata0))
+        return static_cast<void*>(this);
+    return QWidget::qt_metacast(_clname);
+}
+
+int Chess::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 64)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 64;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 64)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 64;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
