42945defcd75365af05b31a657304afa18837d4e
githubnemanja <githubnemanja@users.noreply.github.com>
2019-01-01 22:07:30 +0100

    Add files via upload

---
+++ Chess/stanje.h
@@ -0,0 +1,12 @@
+#ifndef STANJE_H
+#define STANJE_H
+
+enum class QStanje{
+    beliIgrac_pocetakPoteza,
+    beliIgrac_krajPoteza,
+    crniIgrac_pocetakPoteza,
+    crniIgrac_krajPoteza,
+    krajIgre
+};
+
+#endif // STANJE_H
